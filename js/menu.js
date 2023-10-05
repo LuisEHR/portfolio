a006c90f20462bad5bc0b527feb9184cb9777f5b
LuisEHdzR <luis.enr.hdzr@gmail.com>
2023-10-05 12:41:38 -0600

    Navbar modified

--- js/menu.js
+++ js/menu.js
@@ -2,10 +2,32 @@ const open_btn = document.querySelector('#open_btn');
 const close_btn = document.querySelector('#close_btn');
 const menu = document.querySelector('#menu');
 
+const home_mobile = document.querySelector('#home_mobile');
+const about_mobile = document.querySelector('#about_mobile');
+const portfolio_mobile = document.querySelector('#portfolio_mobile');
+const contact_mobile = document.querySelector('#contact_mobile');
+
+
 open_btn.addEventListener('click', () => {
     menu.classList.remove("hidden");
 })
 
 close_btn.addEventListener('click', () => {
+    menu.classList.add("hidden");
+})
+
+home_mobile.addEventListener('click', () => {
+    menu.classList.add("hidden");
+})
+
+about_mobile.addEventListener('click', () => {
+    menu.classList.add("hidden");
+})
+
+portfolio_mobile.addEventListener('click', () => {
+    menu.classList.add("hidden");
+})
+
+contact_mobile.addEventListener('click', () => {
     menu.classList.add("hidden");
 })
