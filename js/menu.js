36b5832194c3ee7d360fdfb3eb38d9122364eb0a
Luis Hernandez <luis.enr.hdzr@gmail.com>
2023-11-10 12:54:33 -0600

    Navbar modified and ES-EN folder added

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,6 @@
 const btn_language = document.querySelector("#btn-language");
+const es_language = document.querySelector("#es-btn");
+const en_language = document.querySelector("#en-btn");
 const language_dropdown = document.querySelector("#language-dropdown-menu");
 
 btn_language.addEventListener("click", () => {
@@ -7,8 +9,18 @@ btn_language.addEventListener("click", () => {
 
   // Change style btn_language
   if (language_dropdown.classList.contains("hidden")) {
-    btn_language.classList.remove("bg-gray-800", "border-gray-700");
+    btn_language.classList.remove("bg-teal-500/20");
   } else {
-    btn_language.classList.add("bg-gray-800", "border-gray-700");
+    btn_language.classList.add("bg-teal-500/20");
   }
+});
+
+es_language.addEventListener("click", () => {
+  // Add hidden class
+  language_dropdown.classList.add("hidden");
+});
+
+en_language.addEventListener("click", () => {
+  // Add hidden class
+  language_dropdown.classList.add("hidden");
 });
